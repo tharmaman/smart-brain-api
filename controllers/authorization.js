a7976708435f3ae9d72386ab6df0cdc7efc08938
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-10-24 20:10:42 -0400

    added sessions

---
+++ controllers/authorization.js
@@ -0,0 +1,21 @@
+const redisClient = require('./signin').redisClient;
+
+const requireAuth = (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization){
+        console.log('you shall not pass');
+        return res.status(401).json('Unauthorized');
+    }
+    return redisClient.get(authorization, (err, reply) => {
+        if(err || !reply){
+            console.log('you shall not pass');
+            return res.status(401).json('Unauthorized'); 
+        }
+        console.log('you shall pass');
+        return next()
+    })
+}
+
+module.exports = {
+    requireAuth
+}
